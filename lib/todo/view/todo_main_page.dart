d0c7c24b321cab0b47252c3b2d9192022b84e3b5
bagjin-eun <wlsdmsrnfl@naver.com>
2025-07-30 00:18:26 +0900

    setting todo main view

--- lib/todo/view/todo_main_page.dart
+++ lib/todo/view/todo_main_page.dart
@@ -15,10 +15,83 @@ class _TodoMainPageState extends ConsumerState<TodoMainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.scaffoldBackgroundColor,
-      body: SafeArea(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.center,
+                end: Alignment.bottomCenter,
+                colors: [
+                  AppColors.scaffoldBackgroundColor,
+
+                  Color(0xFF191B20),
+                  Color(0xFF191B20),
+                  Color(0xFF191B20),
+                  Color(0xFF191B20),
+                  Color(0xFF2A2D35),
+                ]
+            )
+        ),
+
+        child: SafeArea(
           child: Column(
-        children: [],
-      )),
+            children: [
+              //header widget
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 2.0),
+
+                      child: Icon(
+                        Icons.search_outlined,
+                        color: Colors.white,
+                      ),
+
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 2.0),
+
+                      child: Icon(
+                        Icons.home_outlined,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 2.0),
+
+                      child: Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              //body widget
+              Container(
+
+
+                child: Column(
+
+                  children: [],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: CircleBorder(),
+        backgroundColor: AppColors.floatingActionButtonColor,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
